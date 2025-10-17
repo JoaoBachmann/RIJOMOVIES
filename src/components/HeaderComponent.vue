eeaa3ac778f38a34e1931e2e53b20e5b8087b7da
RicbaronR <ric.baron01@gmail.com>
2025-10-17 11:58:27 -0300

    FEAT: desenvolvendo o header #1

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const opcoes = ref(false)
+</script>
+
+<template>
+  <header>
+    <img src="/public/RiJoMovies.png" alt="RijoMovies Logo" />
+    <button @click="opcoes = true">Filmes</button>
+    <div v-if="opcoes">
+      <select name="paises" id="paises">
+        <!--Aqui vou ter q pegar da api os países. E provavelmente colocar a api no pai e mandar as infos para os filhos-->
+      </select>
+    </div>
+    <button><span class="mdi mdi-account-circle"></span></button>
+    <button>Entrar</button>
+  </header>
+</template>
+
+<style  scoped>
+header {
+  background-color: black;
+
+ 
+}
+</style>
