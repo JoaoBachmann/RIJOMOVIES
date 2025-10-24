6994618cd2195d4d79a2ca586486b506c7766f69
RicbaronR <ric.baron01@gmail.com>
2025-10-24 11:54:44 -0300

    FEAT: Base do header finalizada, opções de países em desenvolvimento #1

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,65 +1,102 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref, onMounted } from 'vue'
+import api from '@/plugins/axios'
 const opcoes = ref(false)
+const paises = ref([])
+const selected = ref(null) // aqui vai o país escolhido
+
+const getCountries = async () => {
+  try {
+    const { data } = await api.get('/configuration/countries')
+    paises.value = data
+  } catch (error) {
+    console.error('Erro ao buscar países:', error)
+  }
+}
+onMounted(() => {
+  getCountries()
+})
 </script>
 
 <template>
   <header>
     <div class="logo_Btn-filmes">
-    <img src="/public/RiJoMovies.png" alt="RijoMovies Logo" />
-    <button @click="opcoes = true">Filmes</button>
+      <img src="/public/RiJoMovies.png" alt="RijoMovies Logo" />
+
+      <div class="filmes-container" @mouseenter="opcoes = true" @mouseleave="opcoes = false">
+        <button>Filmes</button>
+
+        <div class="custom-select" v-if="opcoes">
+          <div class="selected">{{ 'Selecione um país' }}</div>
+          <ul v-show="opcoes" class="options">
+            <li
+              v-for="pais in paises"
+              :key="pais.iso_3166_1"
+              @click="
+                selected = pais.native_name || pais.english_name;
+                opcoes = false
+              "
+            >
+              {{ pais.native_name || pais.english_name }}
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <button>Atores</button>
     </div>
     <div class="search">
       <input type="text" placeholder="Pesquisar..." />
+      <span class="mdi mdi-magnify"></span>
     </div>
     <div class="perfil_Btn-login">
-    <button>Login</button>
-    <button>Entrar</button>
-    </div>
-
-
-
-    <div v-if="opcoes">
-      <select name="paises" id="paises">
-        <!--Aqui vou ter q pegar da api os países. E provavelmente colocar a api no pai e mandar as infos para os filhos-->
-      </select>
+      <button>Login</button>
+      <button>Entrar</button>
     </div>
   </header>
+  <p>{{ selected }}</p>
+  <p>{{ atores }}</p>
 </template>
 
-<style  scoped>
+<style scoped>
 header {
   background-color: black;
   display: flex;
   justify-content: space-between;
 
-  & img{
+  & img {
     height: 70px;
-    margin: 10px 0 5px 30px;
+    margin: 10px 40px 5px 30px;
   }
 
-  & .logo_Btn-filmes{
+  & .logo_Btn-filmes {
     display: flex;
     align-items: center;
-    gap: 60px;
+    gap: 80px;
 
-    & button{
+    & button {
       background: none;
       border: none;
       color: white;
       font-size: 20px;
       cursor: pointer;
     }
-
-    
+    & button:hover {
+      color: rgb(255, 0, 0);
+    }
+    & .filmes-container {
+      & button:first-child {
+        padding: 31px 0;
+      }
+    }
   }
 
-  & .search{
+  & .search {
     display: flex;
     align-items: center;
+    position: relative;
 
-    & input{
+    & input {
       width: 700px;
       height: 40px;
       border-radius: 5px;
@@ -67,28 +104,72 @@ header {
       padding: 5px 10px;
       font-size: 16px;
     }
+
+    & span {
+      font-size: 25px;
+      position: absolute;
+      right: 10px;
+    }
   }
 
-  & .perfil_Btn-login{
+  & .perfil_Btn-login {
     display: flex;
     align-items: center;
     gap: 60px;
     margin: 0 60px 0 0;
+    padding-left: 70px;
 
-    & button{
+    & button {
       background: none;
       border: none;
       color: white;
       font-size: 20px;
       cursor: pointer;
     }
-    & button:hover{
+    & button:hover {
       color: rgb(255, 0, 0);
     }
-    & .perfil{
+    & .perfil {
       font-size: 30px;
     }
   }
- 
+}
+.custom-select {
+  width: 200px;
+  background: black;
+  color: white;
+  border: 1px solid red;
+  border-radius: 5px;
+  position: absolute;
+  cursor: pointer;
+  left: 200px;
+}
+
+.custom-select .selected {
+  padding: 8px 10px;
+}
+
+.custom-select .options {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  background: black;
+  border: 1px solid red;
+  border-radius: 5px;
+  max-height: 200px;
+  overflow-y: auto;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  z-index: 10;
+}
+
+.custom-select .options li {
+  padding: 8px 10px;
+}
+
+.custom-select .options li:hover {
+  background: rgb(40, 40, 40);
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
 import '@mdi/font/css/materialdesignicons.css';
+import '@mdi/font/css/materialdesignicons.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
